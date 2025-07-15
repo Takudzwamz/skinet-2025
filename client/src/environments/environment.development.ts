10075bddab45719a36c26bb52b0b27ad5ef06e03
Takudzwamz <zhout1709@hotmail.com>
2025-07-15 10:16:52 +0200

    feat: Add text input component with validation and styles
    - Implemented a reusable text input component with Angular Material.
    - Added error handling for required and email validation.
    - Created corresponding SCSS file for styling.
    
    feat: Create IsAdmin directive for role-based access control
    
    - Developed a directive to conditionally render elements based on admin status.
    
    feat: Define cart, delivery method, order, and user models
    
    - Created TypeScript models for cart, delivery methods, orders, and users.
    - Included necessary properties and types for each model.
    
    feat: Implement address and payment card pipes for formatting
    
    - Added pipes to format shipping addresses and payment card details for display.
    
    chore: Set up environment configurations for development and production
    
    - Configured environment files for API base URLs and Stripe public key.
    
    chore: Initialize Docker setup for SQL Server and Redis
    
    - Created a Docker Compose file to set up SQL Server and Redis services.
    
    chore: Add SSL certificates for local development
    
    - Included self-signed SSL certificates for secure local development.
    
    chore: Set up TypeScript configuration files
    
    - Configured TypeScript settings for the application and tests.
    
    chore: Initialize Visual Studio solution file
    
    - Created a solution file to manage the API, Core, and Infrastructure projects.

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
+  stripePublicKey: 'pk_test_51Rkin0IVF37OtYBpCPkTigUb0czwUlr2mBAAy84jMuXc1JpHvu40Bj0QRiBmP8sDqEVnTGsYNIPWR0jdsTYQ5AY6003nQXuYGS'
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  baseUrl: 'api/',
+  hubUrl: 'hub/notifications',
+  stripePublicKey: 'pk_test_51Rkin0IVF37OtYBpCPkTigUb0czwUlr2mBAAy84jMuXc1JpHvu40Bj0QRiBmP8sDqEVnTGsYNIPWR0jdsTYQ5AY6003nQXuYGS'
+};
